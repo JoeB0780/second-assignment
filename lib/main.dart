1ca086c23f2f526ce9f8884597a21a451911cfd8
JoeB0780 <joelihimbazwe1@gmail.com>
2024-06-29 15:47:43 +0200

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -55,6 +55,493 @@ class _HomeScreenState extends State<HomeScreen> {
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.green[900], // Dark green color
+              ),
+              accountName: const Text(
+                'Joerw@',
+                style: TextStyle(color: Colors.white),
+              ),
+              accountEmail: const Text(
+                'joerw@.com',
+                style: TextStyle(color: Colors.white70),
+              ),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text(
+                  'JR',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.green[900],
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.login),
+              title: const Text('Sign In'),
+              onTap: () => _onDrawerItemTapped(0),
+            ),
+            ListTile(
+              leading: const Icon(Icons.app_registration),
+              title: const Text('Sign Up'),
+              onTap: () => _onDrawerItemTapped(1),
+            ),
+            ListTile(
+              leading: const Icon(Icons.calculate),
+              title: const Text('Calculator'),
+              onTap: () => _onDrawerItemTapped(2),
+            ),
+            const Divider(),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Settings'),
+              onTap: () {
+                // Navigate to settings screen
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.info),
+              title: const Text('About'),
+              onTap: () {
+                // Navigate to about screen
+              },
+            ),
+          ],
+        ),
+      ),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          _navigateToScreen(index);
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.login),
+            label: 'Sign In',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.app_registration),
+            label: 'Sign Up',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calculate),
+            label: 'Calculator',
+          ),
+        ],
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+        unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.normal),
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+}
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _usernameController = TextEditingController();
+    final _passwordController = TextEditingController();
+
+    void _signIn() {
+      String username = _usernameController.text;
+      String password = _passwordController.text;
+
+      // You can add authentication logic here
+      if (username.isNotEmpty && password.isNotEmpty) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text("Sign In"),
+              content: const Text("Sign In Done"),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text("OK"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    // Navigate to HomeScreen without changing the background color
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => const HomeScreen()),
+                    );
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      }
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign In'),
+        backgroundColor: Colors.green[900], // Dark green color
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                labelText: 'Username',
+                prefixIcon: Icon(Icons.person),
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                prefixIcon: Icon(Icons.lock),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _signIn,
+              child: const Text('Sign In'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _nameController = TextEditingController();
+    String _gender = 'Male';
+    final _phoneController = TextEditingController();
+    final _passwordController = TextEditingController();
+    final _confirmPasswordController = TextEditingController();
+
+    void _signUp() {
+      String name = _nameController.text;
+      String gender = _gender;
+      String phone = _phoneController.text;
+      String password = _passwordController.text;
+      String confirmPassword = _confirmPasswordController.text;
+
+      // You can add signup logic here
+      if (name.isNotEmpty &&
+          gender.isNotEmpty &&
+          phone.isNotEmpty &&
+          password.isNotEmpty &&
+          password == confirmPassword) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text("Sign Up"),
+              content: const Text("Sign Up Successful"),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text("OK"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    // Navigate to SignInScreen without changing the background color
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => const SignInScreen()),
+                    );
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      }
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+        backgroundColor: Colors.green[900], // Dark green color
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                  prefixIcon: Icon(Icons.person),
+                ),
+              ),
+              const SizedBox(height: 20),
+              DropdownButtonFormField<String>(
+                value: _gender,
+                items: ['Male', 'Female', 'Other']
+                    .map((label) => DropdownMenuItem(
+                  child: Text(label),
+                  value: label,
+                ))
+                    .toList(),
+                onChanged: (value) {
+                  _gender = value!;
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Gender',
+                  prefixIcon: Icon(Icons.wc),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _phoneController,
+                decoration: const InputDecoration(
+                  labelText: 'Phone Number',
+                  prefixIcon: Icon(Icons.phone),
+                ),
+                keyboardType: TextInputType.phone,
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _confirmPasswordController,
+                decoration: const InputDecoration(
+                  labelText: 'Confirm Password',
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _signUp,
+                child: const Text('Sign Up'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({Key? key}) : super(key: key);
+
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String _output = "0";
+  String _result = "0";
+  String _operand = "";
+  double num1 = 0;
+  double num2 = 0;
+
+  void _buttonPressed(String buttonText) {
+    if (buttonText == "C") {
+      _output = "0";
+      _result = "0";
+      _operand = "";
+      num1 = 0;
+      num2 = 0;
+    } else if (buttonText == "+" || buttonText == "-" || buttonText == "*" || buttonText == "/") {
+      num1 = double.parse(_output);
+      _operand = buttonText;
+      _result = "0";
+    } else if (buttonText == ".") {
+      if (!_result.contains(".")) {
+        _result += buttonText;
+      }
+    } else if (buttonText == "=") {
+      num2 = double.parse(_output);
+
+      if (_operand == "+") {
+        _result = (num1 + num2).toString();
+      }
+
+      if (_operand == "-") {
+        _result = (num1 - num2).toString();
+      }
+
+      if (_operand == "*") {
+        _result = (num1 * num2).toString();
+      }
+
+      if (_operand == "/") {
+        _result = (num1 / num2).toString();
+      }
+
+      _operand = "";
+    } else {
+      if (_result == "0") {
+        _result = buttonText;
+      } else {
+        _result += buttonText;
+      }
+    }
+
+    setState(() {
+      _output = double.parse(_result).toStringAsFixed(2);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculator'),
+        backgroundColor: Colors.green[900], // Dark green color
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(12.0),
+              alignment: Alignment.bottomRight,
+              child: Text(
+                _output,
+                style: TextStyle(
+                  fontSize: 48.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _buildButton("7"),
+              _buildButton("8"),
+              _buildButton("9"),
+              _buildButton("/"),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _buildButton("4"),
+              _buildButton("5"),
+              _buildButton("6"),
+              _buildButton("*"),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _buildButton("1"),
+              _buildButton("2"),
+              _buildButton("3"),
+              _buildButton("-"),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _buildButton("."),
+              _buildButton("0"),
+              _buildButton("00"),
+              _buildButton("+"),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _buildButton("C"),
+              _buildButton("="),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButton(String buttonText) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      child: ElevatedButton(
+        onPressed: () => _buttonPressed(buttonText),
+        child: Text(
+          buttonText,
+          style: TextStyle(fontSize: 30.0),
+        ),
+      ),
+    );
+  }
+}
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tab and Drawer Navigation Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _screens = [
+    const SignInScreen(),
+    const SignUpScreen(),
+    const CalculatorScreen(),
+  ];
+
+  void _onDrawerItemTapped(int index) {
+    Navigator.pop(context); // Close the drawer
+    _navigateToScreen(index);
+  }
+
+  void _navigateToScreen(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tab and Drawer Navigation Demo'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
             DrawerHeader(
               decoration: BoxDecoration(
                 color: Colors.green[900], // Dark green color
